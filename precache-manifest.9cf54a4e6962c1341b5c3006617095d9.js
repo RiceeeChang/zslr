22b9532fba42e5b8b4deefb1d06f1fc407ebf66d
Rice <riceeechang@gmail.com>
2020-07-19 23:20:59 +0800

    Updates

---
+++ precache-manifest.9cf54a4e6962c1341b5c3006617095d9.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273e35dd6ad8f076578e1d99b713e520",
+    "url": "/zslr/index.html"
+  },
+  {
+    "revision": "81dd7c8c22a5497bd127",
+    "url": "/zslr/static/css/2.190caba9.chunk.css"
+  },
+  {
+    "revision": "67c431bf496d6e429ba1",
+    "url": "/zslr/static/css/main.19fda368.chunk.css"
+  },
+  {
+    "revision": "81dd7c8c22a5497bd127",
+    "url": "/zslr/static/js/2.dbe1bd07.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/zslr/static/js/2.dbe1bd07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67c431bf496d6e429ba1",
+    "url": "/zslr/static/js/main.ebcf9ce2.chunk.js"
+  },
+  {
+    "revision": "bb2591f659bfb970425f",
+    "url": "/zslr/static/js/runtime-main.191d050c.js"
+  },
+  {
+    "revision": "4f46004398ac99ba78b54db1eb660075",
+    "url": "/zslr/static/media/01.4f460043.png"
+  },
+  {
+    "revision": "34fa44b1446b0d16a23c82777fb778a2",
+    "url": "/zslr/static/media/01B.34fa44b1.png"
+  },
+  {
+    "revision": "17c4f0ceec7efe104f55ef6dd9849bc0",
+    "url": "/zslr/static/media/02.17c4f0ce.png"
+  },
+  {
+    "revision": "36a473f57c6a2e3b2f933973367c28f4",
+    "url": "/zslr/static/media/02B.36a473f5.png"
+  },
+  {
+    "revision": "bc3f2de495878bcd66862a71f28568b9",
+    "url": "/zslr/static/media/03.bc3f2de4.png"
+  },
+  {
+    "revision": "810b8dc9c5256436f5987024e69622ec",
+    "url": "/zslr/static/media/03B.810b8dc9.png"
+  },
+  {
+    "revision": "3639a48231e58f5d0371bc0303d5626e",
+    "url": "/zslr/static/media/04.3639a482.png"
+  },
+  {
+    "revision": "70c8f0a8f383dca047ea503840f95837",
+    "url": "/zslr/static/media/04B.70c8f0a8.png"
+  },
+  {
+    "revision": "53479e7671066890aabe3925312faeb6",
+    "url": "/zslr/static/media/05.53479e76.png"
+  },
+  {
+    "revision": "fe1e46fb0116471272574ac11eb35042",
+    "url": "/zslr/static/media/05B.fe1e46fb.png"
+  },
+  {
+    "revision": "547a5131df701bd8e9dc84e2798b37cb",
+    "url": "/zslr/static/media/06.547a5131.png"
+  },
+  {
+    "revision": "7673d6aede0f889d40912818b5b24ebf",
+    "url": "/zslr/static/media/06B.7673d6ae.png"
+  },
+  {
+    "revision": "e188abf6eb7abee3a6f904f1db68a8cb",
+    "url": "/zslr/static/media/07.e188abf6.png"
+  },
+  {
+    "revision": "66e66837c63dad12f1758e63d9fb2af5",
+    "url": "/zslr/static/media/07B.66e66837.png"
+  },
+  {
+    "revision": "6f3e6405dbfaee97c25a66c68b684a51",
+    "url": "/zslr/static/media/08.6f3e6405.png"
+  },
+  {
+    "revision": "adeeb253b6616df1f87f6b9ea2f21222",
+    "url": "/zslr/static/media/08B.adeeb253.png"
+  },
+  {
+    "revision": "9769827bddc80918923a5beb743077b3",
+    "url": "/zslr/static/media/09.9769827b.png"
+  },
+  {
+    "revision": "c9b5a2210543fbce71e1d8b0c5ed8275",
+    "url": "/zslr/static/media/09B.c9b5a221.png"
+  },
+  {
+    "revision": "d7e5af74d9125cfa212c7b2af33ef5f8",
+    "url": "/zslr/static/media/10.d7e5af74.png"
+  },
+  {
+    "revision": "abc526537a108ec2c75c832835fc7e7b",
+    "url": "/zslr/static/media/10B.abc52653.png"
+  },
+  {
+    "revision": "155a48488f74a190d8413405349e4560",
+    "url": "/zslr/static/media/11.155a4848.png"
+  },
+  {
+    "revision": "e2517a72ba8de605105c191169e0d4ed",
+    "url": "/zslr/static/media/11B.e2517a72.png"
+  },
+  {
+    "revision": "503ed5a0a6f4493f36c56f1c40c0e4e9",
+    "url": "/zslr/static/media/12.503ed5a0.png"
+  },
+  {
+    "revision": "09c261cccaa5caa86118c4f27614684f",
+    "url": "/zslr/static/media/12B.09c261cc.png"
+  },
+  {
+    "revision": "dfe3c564b1e3a68dece0f2b124781ef0",
+    "url": "/zslr/static/media/Copperplate Light.dfe3c564.ttf"
+  },
+  {
+    "revision": "4757823f9beaff1b1152f45e1a40a6d5",
+    "url": "/zslr/static/media/Copperplate.4757823f.ttf"
+  },
+  {
+    "revision": "d53160535f3745d3e66ac19deda21d23",
+    "url": "/zslr/static/media/GenWanMin-L.d5316053.ttc"
+  },
+  {
+    "revision": "c13fa17c3a741861e9d7bcd2cc53768e",
+    "url": "/zslr/static/media/GenWanMin-M.c13fa17c.ttc"
+  },
+  {
+    "revision": "8cf94693c055e2ed378deaedd3c8669a",
+    "url": "/zslr/static/media/GenWanMin-R.8cf94693.ttc"
+  },
+  {
+    "revision": "f80a3a7ed69d0c60e2b684df012c6318",
+    "url": "/zslr/static/media/GenWanMin-SB.f80a3a7e.ttc"
+  },
+  {
+    "revision": "6bb7f4aa4810636c36301028e0b07520",
+    "url": "/zslr/static/media/cooperation.6bb7f4aa.png"
+  },
+  {
+    "revision": "7542496dc4ccc7b67ecf0e5104f6bd4d",
+    "url": "/zslr/static/media/history.7542496d.png"
+  },
+  {
+    "revision": "2ffbe286790b6835101e6ea82b11ab2a",
+    "url": "/zslr/static/media/opening.2ffbe286.png"
+  },
+  {
+    "revision": "df9af5b0375a65aced1499c83dae42a9",
+    "url": "/zslr/static/media/pro-giant.df9af5b0.png"
+  },
+  {
+    "revision": "fe753869ba29c95280b173559f924b78",
+    "url": "/zslr/static/media/team1.fe753869.png"
+  },
+  {
+    "revision": "382794dbbd9f15e46998d12caacc4ed7",
+    "url": "/zslr/static/media/team2.382794db.png"
+  },
+  {
+    "revision": "e1105a8625141e6df40058de90832330",
+    "url": "/zslr/static/media/team3.e1105a86.png"
+  },
+  {
+    "revision": "e00bc1bedd5fd81f2d8639af84fecb2e",
+    "url": "/zslr/static/media/公共設施01.e00bc1be.png"
+  },
+  {
+    "revision": "44f5111e4da49c1010bd2ff69e0ba0f7",
+    "url": "/zslr/static/media/公共設施02.44f5111e.png"
+  },
+  {
+    "revision": "913dfd2d8d250ac17ec86b5b0e049323",
+    "url": "/zslr/static/media/六國景觀設計有限公司.913dfd2d.png"
+  },
+  {
+    "revision": "aca26c5a97fcbae08aaa88423b6a7631",
+    "url": "/zslr/static/media/十方聯合建築師事務所.aca26c5a.png"
+  },
+  {
+    "revision": "b9da692bd16f8d7cfd26060290b5c13e",
+    "url": "/zslr/static/media/國際盛宴.b9da692b.png"
+  },
+  {
+    "revision": "2e0952147c7e0def2918e05e5555e357",
+    "url": "/zslr/static/media/建材設備01.2e095214.png"
+  },
+  {
+    "revision": "4031c3eb148d24c7bcf705f7453acbc3",
+    "url": "/zslr/static/media/建材設備02.4031c3eb.png"
+  },
+  {
+    "revision": "223941179d76e300b3e0254b870a94e1",
+    "url": "/zslr/static/media/建材設備03.22394117.png"
+  },
+  {
+    "revision": "4886119b843825f0e7f42467dae13c80",
+    "url": "/zslr/static/media/建材設備04.4886119b.png"
+  },
+  {
+    "revision": "50ba051f4656c297d3320dd8119b5b8b",
+    "url": "/zslr/static/media/建材設備05.50ba051f.png"
+  },
+  {
+    "revision": "7475d8c3310be2766f6706f8146a469d",
+    "url": "/zslr/static/media/建材設備06.7475d8c3.png"
+  },
+  {
+    "revision": "18fb1dafec9d6aa82e4f6ea353f3301f",
+    "url": "/zslr/static/media/建材設備07.18fb1daf.png"
+  },
+  {
+    "revision": "d42e747eee97039a21e96434c1920be8",
+    "url": "/zslr/static/media/建材設備08.d42e747e.png"
+  },
+  {
+    "revision": "d33ebe7814e10f212bff0d4cbef3d86f",
+    "url": "/zslr/static/media/文創氛圍.d33ebe78.png"
+  },
+  {
+    "revision": "fa814450094eb1c904f65378f99eee9b",
+    "url": "/zslr/static/media/文化染整.fa814450.png"
+  },
+  {
+    "revision": "5861f1b5248dd5a390d8011d60f389a0",
+    "url": "/zslr/static/media/日常散策.5861f1b5.png"
+  },
+  {
+    "revision": "4ad29692f33311c77e4df100eea66603",
+    "url": "/zslr/static/media/未來啟航.4ad29692.png"
+  },
+  {
+    "revision": "ffbb47428a119b5f988bb359aac6d0a4",
+    "url": "/zslr/static/media/永興土木聯合技師事務所.ffbb4742.png"
+  },
+  {
+    "revision": "9cfc3233fbed928081a321e02f8d0200",
+    "url": "/zslr/static/media/生活價值.9cfc3233.png"
+  },
+  {
+    "revision": "af9f078a912f8dce9e3a3cbb51966871",
+    "url": "/zslr/static/media/生活為名.af9f078a.png"
+  }
+]);
